bac1e77f50d343d3e1f564d3b1f83a899fe8f644
milotr <minhlong010122@gmail.com>
2020-06-22 10:03:37 +0300

    New files

--- testing.py
+++ testing.py
@@ -1,1 +1,13 @@
-print (1^5)
+def readint(prompt, min, max):
+    print(prompt)
+    number = input()
+    try:
+        return number if number < 10 and number > -10 else number
+    except:
+        print("something is wrong")
+    return None
+
+
+v = readint("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
